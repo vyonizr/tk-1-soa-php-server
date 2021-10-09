884ae51c4e2495884572eda360c0b4dbfaac7ea8
Fitrahtur Rahman <fitrahtur@gmail.com>
2021-10-09 09:32:17 +0700

    initial commit server

--- web/index.php
+++ web/index.php
@@ -2,6 +2,9 @@
 
 require('../vendor/autoload.php');
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
@@ -15,11 +18,64 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-// Our web handlers
+class Response {
+  public function __construct($status, $data) {
+    $this->status = $status;
+    $this->data = $data;
+  }
+}
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+  $obj = (object) array('status' => 'OK');
+  return $app->json($obj, 200);
+});
+
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+  array(
+  'pdo.server' => array(
+      'driver'   => 'mysql',
+      'user' => $_ENV['DB_USER'],
+      'password' => $_ENV['DB_PASSWORD'],
+      'host' => $_ENV['CLEARDB_DATABASE_URL'],
+      'port' => (int) $_ENV['DB_PORT'],
+      'dbname' => $_ENV['DB_NAME']
+      )
+  )
+);
+
+$app->get('/couriers', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT id, name FROM couriers');
+  $st->execute();
+
+  $names = [];
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $names[] = $row;
+  }
+
+  $obj = ['status' => 'success','data' => $names];
+  return $app->json($obj, 200);
+});
+
+$app->get('/couriers/{id}', function($id) use($app) {
+  $st = $app['pdo']->prepare("SELECT * FROM shipments WHERE id={$id}");
+  $st->execute();
+
+  $services = [];
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $services[] = $row;
+  }
+
+  $obj = ['status' => 'success','data' => $services];
+  return $app->json($obj, 200)->setEncodingOptions(JSON_NUMERIC_CHECK);
+});
+
+$app->error(function (\Exception $e) use ($app) {
+  if ($e instanceof NotFoundHttpException) {
+    return $app->json(array('error' => 'Page Not Found'), 404);
+  }
+
+  $code = ($e instanceof HttpException) ? $e->getStatusCode() : 500;
+  return $app->json(array('error' => $e->getMessage()), $code);
 });
 
 $app->run();
